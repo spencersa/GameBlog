6da3f610ed843f0145c9d3b72b69699ebfa2c15f
spencersa <spencersa715@gmail.com>
2017-02-09 20:19:52 -0600

    create header and footer and better nav bar

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,10 @@
-// Write your Javascript code.
+$(document).ready(function () {
+
+    $.get("../shared/navigation.html", function (data) {
+        $("#navigation-placeholder").replaceWith(data);
+    });
+
+    $.get("../shared/footer.html", function (data) {
+        $("#footer-placeholder").replaceWith(data);
+    });
+});
